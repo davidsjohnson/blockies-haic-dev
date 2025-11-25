44ae78615f07715fbb60b0158e5011de35a806f0
David Johnson <djohnson@techfak.uni-bielefeld.de>
2025-11-25 15:54:50 +0100

    Updates Setup Config

--- blockies/__init__.py
+++ blockies/__init__.py
@@ -6,6 +6,7 @@ __pdoc__ = {
 
 from blockies.blocky_bias import (
     BlockySampler,
+    BlockyHighVarSampler,
 )
 from blockies.blender import render
 from blockies.scene_parameters import SceneParameters
